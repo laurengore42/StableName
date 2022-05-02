6bb12247a4088749050ccecb4410b479fcf83f60
Lauren Gore <laurengore42@gmail.com>
2022-05-02 23:51:31 +0100

    knock over riderlist for now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { RiderlistHorseComponent } from './components/riderlist/horse/horse.component';
-import { RiderlistRiderComponent } from './components/riderlist/rider/rider.component';
-import { RiderlistComponent } from './components/riderlist/riderlist.component';
 import { ScorelistHorseComponent } from './components/scorelist/horse/horse.component';
 import { ScorelistComponent } from './components/scorelist/scorelist.component';
 import { ScorelistRiderComponent } from './components/scorelist/rider/rider.component';
@@ -14,9 +11,6 @@ import { ScorelistScoretableComponent } from './components/scorelist/scoretable/
 @NgModule({
   declarations: [
     AppComponent,
-    RiderlistHorseComponent,
-    RiderlistRiderComponent,
-    RiderlistComponent,
     ScorelistHorseComponent,
     ScorelistComponent,
     ScorelistRiderComponent,
