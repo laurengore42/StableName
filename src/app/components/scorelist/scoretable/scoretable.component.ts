7af98222624d2ab76865c6b3807bb02327f68aa3
laurengore89 <laurengore89@gmail.com>
2025-05-06 12:35:30 +0100

    floating poooint

--- src/app/components/scorelist/scoretable/scoretable.component.ts
+++ src/app/components/scorelist/scoretable/scoretable.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 
-import { Dnf } from 'src/app/enums';
-import { Horse, Score, Competition, EventSeries } from 'src/app/models';
+import { Score } from 'src/app/models';
 import { DbService } from 'src/app/services';
 
 @Component({
@@ -18,34 +17,42 @@ export class ScorelistScoretableComponent implements OnInit {
     }
 
     ngOnInit() {
-        const eventserieses = this.dbService.EventSerieses;
-        const competitions = this.dbService.Competitions;
-        const horse = this.dbService.GetHorse(this.horsefei);
+      const eventserieses = this.dbService.EventSerieses;
+      const competitions = this.dbService.Competitions;
+      const horse = this.dbService.GetHorse(this.horsefei);
 
-        let scores: Score[] = horse.scores.filter(s => s.Rider.Fei === this.riderfei && s.Result.p !== 'PEND').sort((a, b) => { if (a.Competition.Fei > b.Competition.Fei) { return -1; } else if (b.Competition.Fei > a.Competition.Fei) { return 1; } return 0; });
+      let scores: Score[] = horse.scores
+        .filter(
+          s => s.Rider.Fei === this.riderfei && s.Result.p !== 'PEND'
+        ).sort(
+          (a, b) => {
+            if (a.Competition.Fei > b.Competition.Fei) { return -1; }
+            else if (b.Competition.Fei > a.Competition.Fei) { return 1; }
+            return 0;
+          });
 
-        this.scoresShow = scores
-          .map(
-            s => eventserieses.find(
-              e => e.Id === competitions.find(
-                c => c.Fei === s.Competition.Fei
-              ).EventSeries
-            ).Name + '\xa0' + competitions.find(
+      this.scoresShow = scores
+        .map(
+          s => eventserieses.find(
+            e => e.Id === competitions.find(
               c => c.Fei === s.Competition.Fei
-            ).Year + ':' + (
-              s.Dnf
-                ? s.DnfDescription 
-                : s.Result.p + ': ' + s.Result.a + ' | ' + (
-                  s.Result.b + s.Result.c
-                ) + ' | ' + (
-                  s.Result.d + s.Result.e + (
-                    s.Result.f === undefined ? 0 : s.Result.f
-                  ) + (
-                    s.Result.g === undefined ? 0 : s.Result.g
-                  )
+            ).EventSeries
+          ).Name + '\xa0' + competitions.find(
+            c => c.Fei === s.Competition.Fei
+          ).Year + ':' + (
+            s.Dnf
+              ? s.DnfDescription 
+              : s.Result.p + ': ' + s.Result.a + ' | ' + (
+                s.Result.b + s.Result.c
+              ) + ' | ' + (
+                s.Result.d + s.Result.e + (
+                  s.Result.f === undefined ? 0 : s.Result.f
+                ) + (
+                  s.Result.g === undefined ? 0 : s.Result.g
                 )
-              )
-            );
+              ).toFixed(1)
+            )
+          );
     }
 
 }
