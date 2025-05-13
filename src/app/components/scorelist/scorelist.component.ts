ff389f82da4c162c2bda5edaa451370e35ff3dcc
laurengore89 <laurengore89@gmail.com>
2025-05-13 22:26:53 +0100

    add cross-country mode

--- src/app/components/scorelist/scorelist.component.ts
+++ src/app/components/scorelist/scorelist.component.ts
@@ -40,16 +40,22 @@ export class ScorelistComponent implements OnInit {
             .map(s => new ScoreHorseRider(
                 s,
                 horses.find(h => h.Fei === s.Horse.Fei), riders.find(r => r.Fei === s.Rider.Fei)
-            ))
-            // TO DO figure out how to determine if it's Saturday or Sunday
-            .sort(ScoreHorseRider.sortByResult);
-            //.sort(ScoreHorseRider.sortByDrawOrder);
+            ));
+
+        if (!this.isStatic || (this.scoresShow.some(s => s.score.Result.b > 0 || s.score.Result.c > 0)))
+        {
+          this.scoresShow = this.scoresShow.sort(ScoreHorseRider.sortByResult);
+        }
+        else 
+        {
+          this.scoresShow = this.scoresShow.sort(ScoreHorseRider.sortByDrawOrder);
+        }
 
         this.scoresShow.map(shr => {
             const riderScores = scores.filter(s => s.Rider.Fei === shr.rider.Fei);
             const uniqueHorseNames = [...new Set(Horse.sortHorsesByRecent(riderScores.map(s => horses.find(h => h.Fei === s.Horse.Fei)), riderScores).map(h => h.Name.indexOf('(') > -1 ? h.Name.substring(0, h.Name.indexOf('(') - 1) : h.Name))];
 
-            shr.rider.horseList = uniqueHorseNames.length < 11 ? uniqueHorseNames.join(', ') : uniqueHorseNames.slice(0, 10).join(', ') + ', and others';
+            shr.rider.horseList = uniqueHorseNames.length <= 10 ? uniqueHorseNames.join(', ') : uniqueHorseNames.slice(0, 10).join(', ') + ', and others';
         });
 
         this.comps = this.dbService.Competitions;
