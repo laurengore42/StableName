b59914b0ff755a2663e556eac1df82654d98409c
Lauren Gore <lauren.gore@cantarus.com>
2025-06-10 17:01:05 +0100

    more boekelo, spelled right this time

--- src/app/enums/level.ts
+++ src/app/enums/level.ts
@@ -1,9 +1,11 @@
 export enum Level {
-    '5L' = 'CCI5*-L', // the old CCI 4*
+    '5L' = 'CCI5*-L', // the old CCI 4*, now includes the Olympics
     '4L' = 'CCI4*-L', //         CCI 3*
     '3L' = 'CCI3*-L', //         CCI 2*
     '2L' = 'CCI2*-L', //         CCI 1*
     '4S' = 'CCI4*-S', // the old CIC 3*
     '3S' = 'CCI3*-S', //         CIC 2*
     '2S' = 'CCI2*-S', //         CIC 1*
+    'O4' = 'CCIO4*-L', // teams contest. mainly just Boekelo
+    'P4' = 'CCIO4*-S', // teams contest short, like Aachen
 }
